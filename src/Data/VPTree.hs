f5669b27a3170b20bbdd299c7321bf9c512a1c39
ocramz <zocca.marco@gmail.com>
2020-05-19 10:01:33 +0200

    why tf does selectVP hang

--- src/Data/VPTree.hs
+++ src/Data/VPTree.hs
@@ -1,23 +1,26 @@
 {-# language BangPatterns #-}
 {-# language DeriveFunctor, DeriveFoldable, DeriveTraversable #-}
 {-# language LambdaCase #-}
-{-# options_ghc -Wno-unused-imports #-}
+{-# options_ghc -Wno-unused-imports -Wno-type-defaults -Wno-name-shadowing #-}
 {- | Vantage point trees
 
 Data structures and algorithms for nearest neighbor search in general metric spaces - P. N. Yianilos
 
 http://web.cs.iastate.edu/~honavar/nndatastructures.pdf
 -}
-module Data.VPTree (VPTree, build) where
+module Data.VPTree
+  (VPTree, build)
+  where
 
 import Data.Foldable (foldlM)
 import Control.Monad.ST (ST, runST)
 import Data.List (partition)
+import Data.Maybe (fromMaybe)
 
 -- deepseq
 import Control.DeepSeq (NFData (rnf))
 -- mwc-probability
-import System.Random.MWC.Probability (Gen, Prob, withSystemRandom, asGenST)
+import System.Random.MWC.Probability (Gen, Prob, withSystemRandom, asGenST, asGenIO, GenIO)
 -- primitive
 import Control.Monad.Primitive (PrimMonad(..), PrimState)
 -- sampling
@@ -25,7 +28,7 @@ import Numeric.Sampling (sample)
 -- --transformers
 -- import Control.Monad.Trans.State.Lazy (StateT, get, put, execStateT)
 -- vector
-import Data.Vector (Vector, fromList, thaw, freeze, (!))
+import qualified Data.Vector as V (Vector, map, replicate, partition, zipWith, head, fromList, thaw, freeze, (!))
 import Data.Vector.Generic.Mutable (MVector)
 -- vector-algorithms
 import qualified Data.Vector.Algorithms.Merge as V (sort, Comparison)
@@ -36,96 +39,112 @@ data VPTree d a = Branch {-# UNPACK #-} !d !a !(VPTree d a) !(VPTree d a)
                 deriving (Show, Functor, Foldable, Traversable)
 
 
-nearest distf x = go
-  where go = \case
-          Branch tau v ll rr ->
-            let d = distf x v
-            in
-              if d < tau
-              then go ll
-              else go rr
+-- nearest distf x = go
+--   where go = \case
+--           Branch tau v ll rr ->
+--             let d = distf x v
+--             in
+--               if d < tau
+--               then go ll
+--               else go rr
 
 
 data P = P Double Double deriving (Show)
-ps = [P 0 1, P 1 2, P 3 4, P 2 4, P (- 2) 3, P (-10) 2, P 4 3, P 10 10, P 20 2]
+pps :: V.Vector P
+pps = V.fromList [P 0 1, P 1 2, P 3 4, P 2 4, P (- 2) 3, P (-10) 2, P 4 3, P 10 10, P 20 2]
 distp :: P -> P -> Double
-distp (P x1 y1) (P x2 y2) = sqrt $ x1*x1 + y2*y2
+distp (P x1 y1) (P x2 y2) = sqrt $ x1*x2 + y1*y2
 
 
-build' :: (Floating d, Ord d) =>
-          (a -> a -> d) -> Int -> [a] -> IO (VPTree d a)
-build' df n xs = withST (build df n xs)
+-- build' :: (Floating d, Ord d) =>
+--           (a -> a -> d) -> Float -> V.Vector a -> IO (VPTree d a)
+-- build' df p xs = withST (build df p xs)
+
+build' :: (RealFrac b, Floating d, Show a, Ord d) =>
+          (a -> a -> d) -> b -> V.Vector a -> IO (VPTree d a)
+build' df p xs = withIO $ build df p xs
+
 
 -- | Build a 'VPTree'
-build :: (PrimMonad m, Floating d, Ord d) =>
+build :: (PrimMonad m, RealFrac b, Floating d, Ord d) =>
          (a -> a -> d) -- ^ Distance function
-      -> Int -- ^ Size of random sample
-      -> [a] -- ^ Dataset
-      -> Gen (PrimState m)
-      -> m (VPTree d a)
-build distf n xs gen = do
-  vp <- selectVP distf n xs gen
+      -> b -- ^ Proportion of dataset to sample
+      -> V.Vector a -- ^ Dataset
+      -> Gen (PrimState m) -> m (VPTree d a)
+build distf prop xs gen = do
+  vp <- selectVP distf prop xs gen
+  -- putStrLn $ show vp -- debug
   let
-    nxs = length xs
-    branch l | length l <= 1 = pure Tip
-             | otherwise = build distf n l gen
-    (mu, _) = medianDist distf nxs vp xs
-    (ll, rr) = partition (\x -> distf x vp < mu) xs
+    branch l | length l <= 2 = pure Tip
+             | otherwise = build distf prop l gen
+    (mu, _) = medianDist distf vp xs
+    (ll, rr) = V.partition (\x -> distf x vp < mu) xs
+  -- putStrLn $ unwords ["LL :", show ll]
+  -- putStrLn $ unwords ["RR :", show rr]
   ltree <- branch ll
   rtree <- branch rr
   pure $ Branch mu vp ltree rtree
 
 -- | Select a vantage point
-selectVP :: (PrimMonad m, Ord d, Floating d) =>
+selectVP :: (PrimMonad m, RealFrac b, Foldable f, Ord d, Floating d) =>
             (a -> a -> d) -- ^ distance function
-         -> Int -- ^ size of random sample
-         -> [a] -- ^ dataset
+         -> b -- ^ proportion of dataset to sample
+         -> f a -- ^ dataset
          -> Gen (PrimState m)
          -> m a
-selectVP distf n sset gen = do
-  ps <- sample' n sset gen
-  let pbest = head ps
-  snd <$> foldlM pickMu (0, pbest) ps
+selectVP distf prop sset gen = do
+  ps <- sampleV n sset gen
+  let pstart = V.head ps
+  snd <$> foldlM pickMu (0, pstart) ps
   where
+    n = floor (prop * fromIntegral ndata)
+    ndata = length sset -- size of dataset at current level
     pickMu (best, pcurr) p = do
-      ds <- sample' n sset gen
-      let (mu, dists) = medianDist distf n p ds
-          spread = variance n dists (repeat mu)
+      ds <- sampleV n sset gen
+      let (mu, dists) = medianDist distf p ds
+          spread = variance dists (V.replicate ndata mu)
       if spread > best
         then pure (spread, p)
         else pickMu (spread, pcurr) p
 
-sample' :: PrimMonad m => Int -> [a] -> Gen (PrimState m) -> m [a]
-sample' n xs g = do
-  sm <- sample n xs g
-  case sm of
-    Nothing -> pure xs
-    Just s  -> pure s
+-- | Sample _without_ replacement. Returns empty list if we ask for too many samples
+sampleV :: (PrimMonad m, Foldable f) =>
+           Int -- ^ Size of sample
+        -> f a -> Gen (PrimState m) -> m (V.Vector a)
+sampleV n xs g = V.fromList . fromMaybe [] <$> sample n xs g
+{-# INLINE sampleV #-}
 
-medianDist :: Ord d => (t -> p -> d) -> Int -> p -> [t] -> (d, [d])
-medianDist distf n p ds = (mu, dists)
+medianDist :: Ord d => (t -> p -> d) -> p -> V.Vector t -> (d, V.Vector d)
+medianDist distf p ds = (mu, dists)
   where
-    mu = median n dists
-    dists = map (`distf` p) ds
+    mu = median dists
+    dists = V.map (`distf` p) ds
+{-# INLINE medianDist #-}
 
-median :: Ord a => Int -> [a] -> a
-median n xs = sort xsl ! floor (fromIntegral n / 2)
-  where xsl = fromList xs
+median :: Ord a => V.Vector a -> a
+median xs = sortV xs V.! floor (fromIntegral n / 2)
+  where n = length xs
+{-# INLINE median #-}
 
-variance :: (Floating a) => Int -> [a] -> [a] -> a
-variance n xs mu = mean n $ zipWith sqdiff xs mu
+variance :: (Floating a) => V.Vector a -> V.Vector a -> a
+variance xs mu = mean $ V.zipWith sqdiff xs mu
   where
     sqdiff x y = (x - y) ** 2
+{-# INLINE variance #-}
 
-mean :: (Foldable t, Fractional a) => Int -> t a -> a
-mean n xs = sum xs / fromIntegral n
+mean :: (Fractional a) => V.Vector a -> a
+mean xs = sum xs / fromIntegral (length xs)
+{-# INLINE mean #-}
 
-sort :: Ord a => Vector a -> Vector a 
-sort v = runST $ do
-  vm <- thaw v
+sortV :: Ord a => V.Vector a -> V.Vector a
+sortV v = runST $ do
+  vm <- V.thaw v
   V.sort vm
-  freeze vm
+  V.freeze vm
+{-# INLINE sortV #-}
 
+withIO :: (GenIO -> IO a) -> IO a
+withIO = withSystemRandom . asGenIO
 
 withST :: (Gen s -> ST s a)
        -> IO a
